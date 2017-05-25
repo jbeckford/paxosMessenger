6fcd30b9d4b3ddd5d7339a07a6a1ef2e84bf1818
Jesus Beckford <jesus_beckford@beckfordinteractive.com>
2017-05-25 14:43:51 -0400

    Clean up unused imports.

--- messenger/src/main/java/com/jbeckford/paxos/messenger/service/MessageService.java
+++ messenger/src/main/java/com/jbeckford/paxos/messenger/service/MessageService.java
@@ -1,7 +1,5 @@
 package com.jbeckford.paxos.messenger.service;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import com.jbeckford.paxos.messenger.model.Message;
